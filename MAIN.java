0d41479d63889266cea617f79d70eb2a3033469b
caiio22 <152224577+caiio22@users.noreply.github.com>
2023-11-28 19:56:13 -0300

    ajustando classe principal

--- MAIN.java
+++ MAIN.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-
+Autor autor1 = new Autor("Robert Kiyosaki e Sharon Lechter");
+Autor autor2 = new Autor("Jane Doe");
 
         
     }
