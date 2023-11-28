dc99b323b7416585362b14ac86cd060ed33f1a54
vivibrindeiro <151478714+vivibrindeiro@users.noreply.github.com>
2023-11-28 23:35:29 +0000

    Atualizado

--- Main.java
+++ Main.java
@@ -4,5 +4,9 @@ public class Main {
 Autor autor1 = new Autor("Robert Kiyosaki e Sharon Lechter");
 Autor autor2 = new Autor("Jane Doe");
 
+Livro livro1 = new Livro("Pai Rico, Pai Pobre", "Finanças pessoais", autor1);
+Livro livro2 = new Livro("Estruturas de Dados em Java", "Programação", autor2);
+biblioteca.adicionarLivro(livro1);       
+biblioteca.adicionarLivro(livro2);
         
     }
